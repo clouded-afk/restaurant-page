8dad0a695c0e58fb2e7c5e47e8b172cc7aa3a7f6
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-07-15 19:42:57 -0400

    Add function to render menu page on button click

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,25 @@
 function renderMenu() {
     const container = document.getElementById("content");
-    container.innerHTML = ""
+
+    const content = document.createElement("div")
+    content.classList.add("menu-content")
+
+    const sidesMenu = document.createElement("div")
+    sidesMenu.classList.add("sides-menu")
+    const sideHeader = document.createElement("h2")
+    sideHeader.textContent = "Bon-Chon"
+    sidesMenu.appendChild(sideHeader)
+
+    const meatMenu = document.createElement("div")
+    meatMenu.classList.add("meat-menu")
+    const meatHeader = document.createElement("h2")
+    meatHeader.textContent = "Meat Selection"
+    meatMenu.appendChild(meatHeader)
+
+    content.appendChild(sidesMenu)
+    content.appendChild(meatMenu)
+
+    container.appendChild(content)
 }
 
 export {
