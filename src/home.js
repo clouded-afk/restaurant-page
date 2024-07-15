110ec13472ea27ae6dd6f4376f05821ceece14b9
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-07-15 19:42:29 -0400

    Refactor code

--- src/home.js
+++ src/home.js
@@ -1,15 +1,19 @@
 function renderHome() {
     const container = document.getElementById("content");
+    const content = document.createElement("div")
+    content.classList.add("home-content")
     
     const headline = document.createElement("div");
     headline.classList.add("headline");
     headline.textContent = "Delicious Korean cuisine straight from the home to your belly!";
-    container.appendChild(headline);
+    content.appendChild(headline);
 
     const description = document.createElement("div");
     description.classList.add("description");
     description.textContent = "If you are looking for authentic Korean BBQ, you have come to the right place! We take pride in our family recipes that have been passed down through multiple generations. Just remember, theres nothing quite like a made from scratch, homecooked meal!"
-    container.appendChild(description)
+    content.appendChild(description)
+
+    container.appendChild(content)
 }
 
 export {
