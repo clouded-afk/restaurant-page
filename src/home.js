df92d9b2dffb7a84ce3c6b5fbd75a92ecc33b3ae
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-07-15 16:23:30 -0400

    Refactor code

--- src/home.js
+++ src/home.js
@@ -1,17 +1,6 @@
 function renderHome() {
-    const header = document.querySelector("header");
     const container = document.getElementById("content");
     
-    const restInfo = document.createElement("div");
-    restInfo.classList.add("restaurant-info");
-
-    const restName = document.createElement("div");
-    restName.classList.add("restaurant-name");
-    restName.textContent = "Mama C's Korean Kitchen";
-    restInfo.appendChild(restName);
-
-    header.appendChild(restInfo);
-
     const headline = document.createElement("div");
     headline.classList.add("headline");
     headline.textContent = "Delicious Korean cuisine straight from the home to your belly!";
