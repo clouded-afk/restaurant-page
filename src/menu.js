86833466dbeacf30119cf9efac0b532c1ad329ad
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-07-16 17:33:37 -0400

    Create functions to populate menu container

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,65 @@
 function renderMenu() {
     const container = document.getElementById("content");
 
-    const content = document.createElement("div")
-    content.classList.add("menu-content")
+    const menuContent = document.createElement("div")
+    menuContent.classList.add("menu-content")
 
-    const sidesMenu = document.createElement("div")
-    sidesMenu.classList.add("sides-menu")
-    const sideHeader = document.createElement("h2")
-    sideHeader.textContent = "Bon-Chon"
-    sidesMenu.appendChild(sideHeader)
+    function createSideMenu() {
+        const sidesMenu = document.createElement("div")
+        sidesMenu.classList.add("sides-menu")
+        const sideHeader = document.createElement("h2")
+        sideHeader.textContent = "Bon-Chon"
+        sidesMenu.appendChild(sideHeader)
 
-    const meatMenu = document.createElement("div")
-    meatMenu.classList.add("meat-menu")
-    const meatHeader = document.createElement("h2")
-    meatHeader.textContent = "Meat Selection"
-    meatMenu.appendChild(meatHeader)
+        const sides = ["Kimchi", "Cucumber Kimchi", "Raddish Kimchi", "Bean Sprouts", "Spinach", "Water Crest", "Fish Cake", "Spicy Ricecake"]
 
-    content.appendChild(sidesMenu)
-    content.appendChild(meatMenu)
+        sides.forEach((side) => {
+            const sideContainer = document.createElement("div")
+            sideContainer.classList.add("side-container")
+            const sideName = document.createElement("h3")
+            sideName.textContent = `${side}`
 
-    container.appendChild(content)
+            const divLine = document.createElement("div")
+            divLine.classList.add("line")
+
+            sideContainer.appendChild(sideName)
+            sideContainer.appendChild(divLine)
+            sidesMenu.append(sideContainer)
+        })
+
+        return sidesMenu
+    }
+
+    function createMeatMenu() {
+        const meatMenu = document.createElement("div")
+        meatMenu.classList.add("meat-menu")
+        const meatHeader = document.createElement("h2")
+        meatHeader.textContent = "Meat Selection"
+        meatMenu.appendChild(meatHeader)
+
+        const meats = ["Brisket", "Bulgogi", "Galbi", "Marinated Sirloin", "Fresh Pork Belly", "Gochjang Pork Belly", "Marinated Spicy Chicken", "Shrimp"]
+
+        meats.forEach((meat) => {
+            const meatContainer = document.createElement("div")
+            meatContainer.classList.add("meat-container")
+            const meatName = document.createElement("h3")
+            meatName.textContent = `${meat}`
+
+            const divLine = document.createElement("div")
+            divLine.classList.add("line")
+
+            meatContainer.appendChild(meatName)
+            meatContainer.appendChild(divLine)
+            meatMenu.appendChild(meatContainer)
+        })
+
+        return meatMenu
+    }
+
+    menuContent.appendChild(createSideMenu())
+    menuContent.appendChild(createMeatMenu())
+
+    container.appendChild(menuContent)
 }
 
 export {
